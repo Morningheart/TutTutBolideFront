cf597a4fc2fc571c000fd5cf1be5936bf9728195
Morningheart <46131687+Morningheart@users.noreply.github.com>
2023-03-10 10:43:27 +0100

    fix: service distance url

--- map/service/serviceAPIDistance.py
+++ map/service/serviceAPIDistance.py
@@ -1,7 +1,7 @@
 import requests
 
 def callDistance(coord1, coord2):
-    res = requests.post("https://tut-tut-bolide-rest.vercel.app/distances", 
+    res = requests.post("https://tut-tut-bolide-rest.vercel.app/distance", 
     json={"coordinates":[
         {"lon": coord1[1], "lat": coord1[0]},
         {"lon": coord2[1], "lat": coord2[0]}   
