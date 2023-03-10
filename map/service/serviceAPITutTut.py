a2f10a9055ce793df03d3f8a4cb2a0197ae1aded
Morningheart <46131687+Morningheart@users.noreply.github.com>
2023-03-10 12:56:27 +0100

    fix: api call

--- map/service/serviceAPITutTut.py
+++ map/service/serviceAPITutTut.py
@@ -2,42 +2,55 @@ from graphqlclient import GraphQLClient
 import requests
 
 clientTutTut = GraphQLClient(endpoint="https://api.chargetrip.io/graphql")
-queryTutTut = """query vehicleListFilter($query: vehicleList!) {
-                    vehicleList(query: $query) {
-                        id
-                        naming {
-                            model
-                            version
-                            edition
-                        }
-                        connectors {
-                            time
-                        }
-                        range {
-                            worst {
-                                combined {
-                                    unit {
-                                        kilometer
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }"""
+queryTutTut = """
+query vehicleListAll {
+  vehicleList (search: $name){
+    naming {
+      make
+      model
+      version
+      edition
+      chargetrip_version
+    }
+    connectors {
+      time
+    }
+    battery {
+      usable_kwh
+      full_kwh
+    }
+    range {
+      chargetrip_range {
+        worst
+      }
+    }
+    media {
+      image {
+        id
+        type
+        url
+        height
+        width
+        thumbnail_url
+        thumbnail_height
+        thumbnail_width
+      }
+    }
+  }
+}
+"""
 clientTutTut.inject_token("5ed1175bad06853b3aa1e492", "x-client-id")
 clientTutTut.inject_token("623996f3c35130073829b252","x-app-id")
 
 
 def getModeleTutTutFromName(name):
     variablesTutTut = {
-        "query": {
-            "search": name,
-            "size": 1
-        }
+        "name": name
     }
     
     # Call API to find the bornes
     dataTutTut = clientTutTut.execute(query=queryTutTut, variables=variablesTutTut)
+    
     return dataTutTut["data"][0]
 
     
